97096a1b52512fffd02e26a783324b056794970c
Arsalan Shemirani <arsalan.shakeri0@gmail.com>
2020-05-19 15:43:24 +0430

    changed tet for new branch

--- get_devices.py
+++ get_devices.py
@@ -36,7 +36,7 @@ def main():
         for device in get_resp.json()["response"]:
             print(f"ID: {device['id']}  IP: {device['managementIpAddress']}")
     else:
-        print(f"Device collection failed with code {get_resp.status_code}")
+        print(f"Device collection failed with error code {get_resp.status_code}")
         print(f"FAILURE BODY: {get_resp.text}")
 
 #    import json; print(json.dumps(get_resp.json(), indent=2))
